0bd84d2e490f35d443d30163a85056033fad139b
Gabriel <orionstelaris04@gmail.com>
2026-05-17 14:55:09 -0300

    chore: remove test files

--- apps/web/test2.ts
+++
@@ -1,5 +0,0 @@
-import { valibotResolver } from '@hookform/resolvers/valibot';
-import * as v from 'valibot';
-
-const Schema = v.pipe(v.object({ a: v.string() }));
-const resolver = valibotResolver(Schema);
